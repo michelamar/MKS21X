77a0fb65c569fea345e357aa36180593dd143250
michelamar <mmarchini@stuy.edu>
2016-11-29 23:28:25 -0500

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,41 @@
+public class Book{
+    private String author;
+    private String title;
+    private String ISBN;
+
+    public Book(){
+    }
+
+    public Book(String a, String t, String i){
+	author = a;
+	title = t;
+	ISBN = i;
+    }
+
+    public String getAuthor(){
+	return author;
+    }
+    public String getTitle(){
+	return title;
+    }
+    public String getISBN(){
+	return ISBN;
+    }
+
+    public void setAuthor(String n){
+	author =  n;
+    }
+    public void setTitle(String n){
+	title = n;
+    }
+    public void setISBN(String n){
+	ISBN = n;
+    }
+
+    public String toString(){
+	return("Author: " +getAuthor() + "Title: " +getTitle()+ "ISBN: " +getISBN());
+    }
+
+    public static void main(String[]args){
+    }
+}
